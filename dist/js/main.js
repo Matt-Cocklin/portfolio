803329650b2d01405be44eb5bfe5e6779002c2e8
matt-cocklin <mnc2603@gmail.com>
2019-06-17 21:38:52 +0100

    working modals for projects

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,5 @@
+// ########## Menu Button ##########
+
 //Select DOM items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
@@ -31,3 +33,27 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// ########## Modals ##########
+
+var modalBtns = [...document.querySelectorAll(".modalBtn")];
+modalBtns.forEach(function(btn) {
+  btn.onclick = function() {
+    var modal = btn.getAttribute("data-modal");
+    document.getElementById(modal).style.display = "block";
+  };
+});
+
+var closeBtns = [...document.querySelectorAll(".closeBtn")];
+closeBtns.forEach(function(btn) {
+  btn.onclick = function() {
+    var modal = btn.closest(".modal");
+    modal.style.display = "none";
+  };
+});
+
+window.onclick = function(event) {
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
+  }
+};
